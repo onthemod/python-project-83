0c9d5d3176cc20d4ebe92c8eece8c4b2937b3042
onthemod <onthemod@yandex.ru>
2023-08-19 02:16:53 +0300

    simplified code and add parameters to querys

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -30,176 +30,121 @@ def urls_page():
         result_info = []
         with psycopg2.connect(DATABASE_URL) as conn:
             with conn.cursor() as curs:
-                curs.execute(f"SELECT id from urls WHERE name='{url_string}'")
+                get_ids_of_url_query = "SELECT id from urls WHERE name= %s;"
+                curs.execute(get_ids_of_url_query, (url_string,))
                 urls_tuples = curs.fetchall()
                 if urls_tuples:
+                    print('страница существует')
                     flash("Страница уже существует", "alert alert-danger")
-                    print(f" Такой вот кортеж-мортеж {urls_tuples[0]}")
                     return redirect(url_for('get_url', id=urls_tuples[0][0]))
-                curs.execute(f"INSERT into urls (name, created_at) VALUES ('{url_string}', NOW())")
+                add_url_query = "INSERT into urls (name, created_at) VALUES (%s, NOW()) returning id;"
+                curs.execute(add_url_query, (url_string,))
+                new_id = curs.fetchone()[0]
                 conn.commit()
                 flash("Страница успешно добавлена", "alert alert-success")
-                return redirect(url_for('index'))
+                return redirect(url_for('get_url', id=new_id))
     
 @app.get('/urls')
 def get_urls():
     messages = get_flashed_messages(with_categories=True)
     print(f"messages = {messages}")
     result_info = []
-    urls_list = make_db_processing(get_urls_list, result_info=result_info)
-    if connection_failed in result_info:
-        flash("Нет соединения с базой данных", "alert alert-danger")
-        return redirect(url_for('index'))
-    return render_template("urls.html", urls=urls_list, messages = messages)
+    with psycopg2.connect(DATABASE_URL) as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM urls order by created_at desc")
+            urls_tuples = cur.fetchall()
+            urls_list= [] 
+            if not urls_tuples:
+                print('empty')
+            else:
+                print(urls_tuples)
+            for url_tuple in urls_tuples:
+                id, name, date = url_tuple
+                get_max_date_query = "SELECT max(created_at), status_code FROM url_checks where url_id=%s group by status_code;"
+                cur.execute(get_max_date_query, (id,))
+                url_check_tuples = cur.fetchall()
+                check_date = ''
+                status = ''
+                if url_check_tuples:
+                    check_date = url_check_tuples[0][0]
+                    status = url_check_tuples[0][1]
+                    if check_date:
+                        check_date = check_date.date()
+                    else:
+                        check_date = ''
+                urls_list.append({'id': id, 'name': name, 'date' : date.date(), 'check_date' : check_date, 'status' : status})
+            return render_template("urls.html", urls=urls_list, messages = messages)
 
 @app.get('/urls/<id>')
 def get_url(id):
     messages = get_flashed_messages(with_categories=True)
     print('before get url data')
     print(id)
-    result = make_db_processing(get_url_data, id)
-    if result:
-        id, name, date = result
-        urls_data = {"id" : id, "name":name, "date":date.date()}
-        result = make_db_processing(get_url_checks, id)
-        url_checks_list = []
-        print(f"url_checks = {result}")
-        if result:
-            for url_check_tuple in result:
-                id, status, h1, title, content, date = url_check_tuple
-                if not status:
-                    status = ''
-                if not h1:
-                    h1 = ''
-                if not title:
-                    title = ''
-                if not content:
-                    content = ''
-                url_checks_list.append({'id': id, 'status' : status,'h1' : h1, 'title': title, 'content':content,'date' : date.date()})
-        return render_template("url.html", url=urls_data, url_checks = url_checks_list, messages = messages)
-    
-    return redirect(url_for('index'))
-    
+    with psycopg2.connect(DATABASE_URL) as conn:
+        with conn.cursor() as cur:
+            get_url_data_query = "SELECT * FROM urls where id=%s ;"
+            cur.execute(get_url_data_query, (id,))
+            urls_tuples = cur.fetchall()
+            id, name, date = urls_tuples[0]
+            urls_data = {"id" : id, "name":name, "date":date.date()}
+            get_url_checks_data= "SELECT id, status_code, h1, title, content, created_at FROM url_checks where url_id=%s order by created_at desc;"
+            cur.execute(get_url_checks_data, (id,))
+            url_checks_tuples = cur.fetchall()
+            url_checks_list = []
+            if url_checks_tuples:
+                for url_check_tuple in url_checks_tuples:
+                    id, status, h1, title, content, date = url_check_tuple
+                    if not status:
+                        status = ''
+                    if not h1:
+                        h1 = ''
+                    if not title:
+                        title = ''
+                    if not content:
+                        content = ''
+                    url_checks_list.append({'id': id, 'status' : status,'h1' : h1, 'title': title, 'content':content,'date' : date.date()})
+            return render_template("url.html", url=urls_data, url_checks = url_checks_list, messages = messages)
+   
 @app.post('/urls/<id>/checks')
 def check_url(id):
     result_info = []
-    result = make_db_processing(get_url_data, id)
-    if result:
-        name = result[1]
-    try:
-        print(f'name = {name}')
-        req = requests.request("GET", name)
-    except:
-        flash("Произошла ошибка при проверке", "alert alert-danger")
-        return get_url(id)
-    html_content = req.text
+    with psycopg2.connect(DATABASE_URL) as conn:
+        with conn.cursor() as cur:
+            get_url_data_query = "SELECT * FROM urls where id=%s ;"
+            cur.execute(get_url_data_query, (id,))
+            urls_tuples = cur.fetchall()
+            print(urls_tuples)
+            if urls_tuples:
+                name = urls_tuples[0][1]
+            try:
+                print(f'name = {name}')
+                req = requests.request("GET", name)
+            except:
+                flash("Произошла ошибка при проверке", "alert alert-danger")
+                return get_url(id)
+            html_content = req.text
     
-    soup = BeautifulSoup(html_content, 'html.parser')
-    h1 = soup.find('h1')
-    if h1:
-        h1 = h1.text
-    else:
-        h1 = ''
-    title = soup.find('title')
-    if title:
-        title = title.text
-    else:
-        title = ''
-    print(f'=======h1 is {h1}')
-    meta_description_tag = soup.find('meta', attrs={'name': 'description'})
-    content = ''
-    if meta_description_tag:
-        content = meta_description_tag.get("content")
-        if not content:
-            content = ''
-
-    params = {'check_id': id, 'status_code': req.status_code, 'title': title, 'h1': h1, 'content': content}
-    make_db_processing(make_check, params, result_info)
-    if page_checked in result_info:
-        flash("Страница успешно проверена", "alert alert-success")
-        return get_url(id)
-
-def post_url(curs, url, result_info = []):
-    curs.execute(f"SELECT id from urls WHERE name='{url}'")
-    urls_tuples = curs.fetchall()
-    print(f"urls tuples = {urls_tuples}")
-    if not urls_tuples:
-        request_string = f"INSERT into urls (name, created_at) VALUES ('{url}', NOW())"
-        print(request_string)
-        curs.execute(f"INSERT into urls (name, created_at) VALUES ('{url}', NOW())")
-        result_info.append(page_added)
-    else:
-        result_info.append(urls_tuples[0][0])
-
-
-def make_db_processing(query_function, params = '', result_info = []):
-    print('making processing')
-    try:
-        conn = psycopg2.connect(DATABASE_URL)
-    except:
-        result_info.append(connection_failed)
-        print("Эксепшен, йо")
-        return 
-    print(f'params in make_db_processing = {params}')
-    with conn.cursor() as curs:
-        query_result = query_function(curs, params, result_info) #посмотреть, как лучше возвращать результат
-        if page_added in result_info or page_checked in result_info:
-            print(result_info)
-            conn.commit()
-    conn.close()
-    return query_result
-
-
-def get_urls_list(cur, params='', result_info=[]):
-    cur.execute("SELECT * FROM urls order by created_at desc")
-    urls_tuples = cur.fetchall()
-    urls_list= [] 
-    if not urls_tuples:
-        print('empty')
-    else:
-        print(urls_tuples)
-    for url_tuple in urls_tuples:
-        id, name, date = url_tuple
-        cur.execute(f"SELECT max(created_at), status_code FROM url_checks where url_id={id} group by status_code")
-        url_check_tuples = cur.fetchall()
-        check_date = ''
-        status = ''
-        if url_check_tuples:
-            check_date = url_check_tuples[0][0]
-            status = url_check_tuples[0][1]
-            if check_date:
-                check_date = check_date.date()
+            soup = BeautifulSoup(html_content, 'html.parser')
+            h1 = soup.find('h1')
+            if h1:
+                h1 = h1.text
             else:
-                check_date = ''
-        urls_list.append({'id': id, 'name': name, 'date' : date.date(), 'check_date' : check_date, 'status' : status})
-    return urls_list
- 
-def make_check(curs, params = '', result_info=[]):
-    print(f'check params = {params}')
-    request_string = f"INSERT into url_checks (url_id, status_code, created_at, h1, title, content) VALUES ('{params['check_id']}', '{params['status_code']}', NOW(),'{params['h1']}','{params['title']}','{params['content']}')"
-    curs.execute(request_string)
-    result_info.append(page_checked)
+                h1 = ''
+            title = soup.find('title')
+            if title:
+                title = title.text
+            else:
+                title = ''
+            print(f'=======h1 is {h1}')
+            meta_description_tag = soup.find('meta', attrs={'name': 'description'})
+            content = ''
+            if meta_description_tag:
+                content = meta_description_tag.get("content")
+                if not content:
+                    content = ''
 
-def get_url_data(cur, params='', result_info=[]):
-    print(f' get url data params {params}')
-    cur.execute(f"SELECT * FROM urls where id={params}")
-    urls_tuples = cur.fetchall()
-    urls_list= [] 
-    if not urls_tuples:
-        print('empty')
-        return None
-    else:
-        print(urls_tuples)
-    return urls_tuples[0]
-    
-def get_url_checks(cur, params='', result_info=[]):
-    cur.execute(f"SELECT id, status_code, h1, title, content, created_at FROM url_checks where url_id={params} order by created_at desc")
-    urls_tuples = cur.fetchall()
-    urls_list= [] 
-    if not urls_tuples:
-        print('empty')
-        return None
-    else:
-        print(f"check tuples = {urls_tuples}")
-    return urls_tuples
-
+            params = {'check_id': id, 'status_code': req.status_code, 'title': title, 'h1': h1, 'content': content}
+            request_string = "INSERT into url_checks (url_id, status_code, created_at, h1, title, content) VALUES (%s, %s, NOW(),%s,%s,%s);"
+            cur.execute(request_string, (params['check_id'],params['status_code'],params['h1'],params['title'],params['content']))
+            flash("Страница успешно проверена", "alert alert-success")
+            return get_url(id)
